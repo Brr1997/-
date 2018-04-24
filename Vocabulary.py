d7dcd8aba908114535c705e9f56f32cf19bc4c61
Brr1997 <36627004+Brr1997@users.noreply.github.com>
2018-04-24 15:53:16 +0300

    Add files via upload

---
+++ Vocabulary.py
@@ -0,0 +1,51 @@
+print ("Welcome to vocabulary")
+q=input ("Enter the word (English): ")
+if q==("family"):
+    print ("семья")
+if q==("parents"):
+    print ("родители")
+if q==("what is your name?"):
+    print ("как вас зовут?")
+if q==("my name is Bahriddin"):
+    print ("меня зовут Бахриддин")
+if q == ("cat"):
+     print("кошка")
+if q == ("apple"):
+     print("яблоко")
+if q == ("how is your mood?"):
+     print("как у вас настроение?")
+if q == ("excellent thank you"):
+     print("отлично спасибо")
+if q == ("boy"):
+     print("мальчик")
+if q == ("girl"):
+     print("девушка")
+if q == ("i am a student"):
+     print("я студент")
+if q == ("who is your teacher?"):
+     print("кто твой учитель?")
+if q == ("our best teacher is Shterenberg"):
+     print("наш лучший учитель - Штеренберг")
+if q == ("horse"):
+      print("лошадь")
+if q == ("lion"):
+     print("лев")
+if q == ("what is the weather today?"):
+     print("какая сегодня погода?")
+if q == ("sun"):
+     print("солнце")
+if q == ("rain"):
+     print("дождь")
+if q == ("today weather is rainy"):
+     print("сегодня погода дождливая")
+if q == ("i love my parents"):
+     print("я люблю своих родителей")
+if q == ("friend"):
+      print("друг")
+if q == ("room"):
+      print("комната")
+if q == ("be silent about great talent"):
+      print("молчать большой талант")
+if q == ("hello world"):
+      print("привет мир")
+print ("Have a nice day *_*")
